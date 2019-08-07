512f76e070b2dff2b8fba75695ef6bd48665690d
SmerchProsto <smerchprosto@gmail.com>
2019-08-07 16:32:28 +0400

    Fixed some small bugs

--- js/app.js
+++ js/app.js
@@ -5,7 +5,37 @@ if (typeof parent.App !== 'undefined')
 else
     App = new function () {
         var fs = require('fs');
+
+        // get the window screen
+        var W = this.W = nw.Window.get();
+
+
+        var parse_path = function (path) {
+            var path_arr, file_name = '', file_exp = '';
+            path_arr = path.split(/[\/\\]/);
+
+
+            file_name = path_arr[path_arr.length - 1];
+            return {
+                path: path_arr,
+                file_name: file_name,
+                exp: file_exp
+            };
+        };
+
+        this.get_file_name = function (path) {
+            return parse_path(path).file_name;
+        };
+
+        // with help node we can read file
         this.read_file = function (path) {
             return fs.readFileSync(path);
         };
+
+        this.write_file = function (path, data) {
+            return fs.writeFileSync(path, data);
+        };
+        /*this.write_file = function (path, data) {
+            return fs.writeFileSync(path, data);
+        };*/
     };
--- js/tabs.js
+++ js/tabs.js
@@ -5,6 +5,7 @@ if (typeof parent.Tabs !== 'undefined')
 else
     Tabs = new function () {
         var tab_list  = {};
+        var selected = '';
 
         var _add_tab = function (path, name) {
             var tab_frame, tab, tab_close, tab_name;
@@ -12,10 +13,14 @@ else
             tab_frame = $create('iframe');
             tab_frame.className = 'frame';
             tab_frame.src = 'html/code_editor.html#'+path;
+            // tab_frame.src = 'package.json#'+path;
 
             tab = $create('span');
             tab.className = 'tab';
             tab.id = path;
+            tab.onclick = function () {
+              Tabs.select(this.id);
+            };
 
             tab_name = $create('span');
             tab_name.innerHTML = name ? name : 'Tab';
@@ -23,7 +28,8 @@ else
             tab_close = $create('span');
             tab_close.className = 'tab_close';
             tab_close.innerHTML = '&#10006';
-            tab_close.onclick = function () {
+            tab_close.onclick = function (e) {
+                e.stopPropagation(); // for stop the work of event
                 tab.parentNode.removeChild(tab);
                 tab_frame.parentNode.removeChild(tab_frame);
                 delete tab_list[tab.id];
@@ -39,8 +45,9 @@ else
                 tab_name: tab_name,
                 tab_frame: tab_frame,
                 tab_close: tab_close
-            }
+            };
 
+            Tabs.select(path);
             return tab_list;
         };
 
@@ -52,5 +59,34 @@ else
             tab.parentNode.removeChild(tab);
             tab_frame.parentNode.removeChild(tab_frame);
             delete tab_list[tab.id];
+            selected = '';
         };
+        
+        this.select = function (id) {
+            if (!tab_list[id]) return;
+            if (selected) {
+                tab_list[selected].tab_frame.style.display = 'none';
+                tab_list[selected].tab.className = tab_list[selected].tab.className.replace(' tab_selected', '');
+            }
+
+            tab_list[id].tab_frame.style.display = 'block';
+            tab_list[id].tab.className += ' tab_selected';
+
+            selected = id;
+        };
+
+        this.selected = function () {
+
+        };
+
+        this.save_selected = function () {
+            if (!selected) return;
+
+            var frame = tab_list[selected].tab_frame.contentWindow;
+
+            if (frame.SAVE) {
+                frame.SAVE();
+            }
+        };
+
     };
